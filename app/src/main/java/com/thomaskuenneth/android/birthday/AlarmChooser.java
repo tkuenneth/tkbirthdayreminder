fdc0fb98722fc28ac50d4bcc10e4d8158e90a30c
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2020-02-01 15:19:07 +0100

    Modernized notification time picker

--- app/src/main/java/com/thomaskuenneth/android/birthday/AlarmChooser.java
+++ app/src/main/java/com/thomaskuenneth/android/birthday/AlarmChooser.java
@@ -1,58 +1,53 @@
 /*
  * AlarmChooser.java
  *
- * TKBirthdayReminder (c) Thomas Künneth 2017
+ * TKBirthdayReminder (c) Thomas Künneth 2017 - 2020
  * Alle Rechte beim Autoren. All rights reserved.
  */
 package com.thomaskuenneth.android.birthday;
 
 
-import android.app.Activity;
-import android.app.TimePickerDialog;
-import android.content.DialogInterface;
+import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.text.format.DateFormat;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.view.View;
 import android.widget.TimePicker;
 
-public class AlarmChooser extends Activity {
+public class AlarmChooser extends DialogPreference {
 
     static final String NOTIFICATION_TIME_HOUR = "notificationTimeHour";
     static final String NOTIFICATION_TIME_MINUTE = "notificationTimeMinute";
 
     private SharedPreferences prefs;
+    private TimePicker picker;
+
+    public AlarmChooser(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        prefs = TKBirthdayReminder.getSharedPreferences(context);
+        setDialogLayoutResource(R.layout.notification_time);
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        prefs = TKBirthdayReminder.getSharedPreferences(this);
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+        int hour = prefs.getInt(NOTIFICATION_TIME_HOUR, 12);
+        int minute = prefs.getInt(NOTIFICATION_TIME_MINUTE, 0);
+        picker = view.findViewById(R.id.notification_time_picker);
+        picker.setHour(hour);
+        picker.setMinute(minute);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        int hour = prefs.getInt(NOTIFICATION_TIME_HOUR, 12);
-        int minute = prefs.getInt(NOTIFICATION_TIME_MINUTE, 0);
-        TimePickerDialog picker = new TimePickerDialog(this,
-                new TimePickerDialog.OnTimeSetListener() {
-
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        SharedPreferences.Editor editor = prefs.edit();
-                        editor.putInt(NOTIFICATION_TIME_HOUR, hourOfDay);
-                        editor.putInt(NOTIFICATION_TIME_MINUTE, minute);
-                        editor.apply();
-                        BootCompleteReceiver.startAlarm(AlarmChooser.this, true);
-                        finish();
-                    }
-                }, hour, minute, DateFormat.is24HourFormat(this));
-        picker.setOnDismissListener(new DialogInterface.OnDismissListener() {
-
-            @Override
-            public void onDismiss(DialogInterface dialog) {
-                finish();
-            }
-        });
-        picker.show();
+    protected void onDialogClosed(boolean positiveResult) {
+        super.onDialogClosed(positiveResult);
+        if (positiveResult) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt(NOTIFICATION_TIME_HOUR, picker.getHour());
+            editor.putInt(NOTIFICATION_TIME_MINUTE, picker.getMinute());
+            editor.apply();
+            BootCompleteReceiver.startAlarm(AlarmChooser.this.getContext(), true);
+        }
     }
 }
